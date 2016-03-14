b9a003b209d140eff4c84ad9a54363dc73f71cec
caioddm <caioduarte.diniz@gmail.com>
2016-03-14 18:32:49 -0500

    [P4] Improvements on VMPool
    Improved VMPool allocated regions management to be more flexible and
    reuse freed regions.

--- P4/P4-provided-code/vm_pool.C
+++ P4/P4-provided-code/vm_pool.C
@@ -35,57 +35,101 @@ VMPool::VMPool(unsigned long _base_address, unsigned long _size, FramePool *_fra
 	pageTable = _page_table;
 	region_index = 0;
 	_page_table->register_vmpool(this); //register the VM Pool on the page table
+	regions[0].base_address = _base_address;
+	regions[0].size = _size;
+	regions[0].available = TRUE;
+	/*Initialize the allocated regions array with a single element representing the entire vm pool as available*/
 }
 
 unsigned long VMPool::allocate(unsigned long _size){
-	if(_size == 0 || region_index >= _MAX_NO_REGIONS){ //check if the size is 0 or if the regions list is full
-		Console::puts("Error when allocating the region!\n");
-		return 0;
-	}else{
-		unsigned long base_addr = base_address;
-		if(region_index > 0){
-			base_addr = regions[region_index - 1]->base_address + regions[region_index - 1]->size; //computes the base address of the region to be allocated			
+	unsigned long base_addr = 0;
+	unsigned long remainingSize = 0;
+	unsigned long sizeToAllocate = _size % PageTable::PAGE_SIZE > 0 ? ((_size / PageTable::PAGE_SIZE) + 1) * PageTable::PAGE_SIZE : _size; //computes the size of the region, rounding to the next multiple of page size
+	int i;
+	for (i = 0; i < region_index; ++i) //iterate over the allocated regions to look if any of the previously allocate ones is available now
+	{	
+		if(regions[i].available == TRUE && regions[i].size >= sizeToAllocate){ //found a region that was released
+			base_addr = regions[i].base_address;
+			if(i < _MAX_NO_REGIONS - 1){ //there is still room to split this region in two if size needed is less than the current size of the region
+				remainingSize = regions[i].size - sizeToAllocate; //compute the remaining size when allocating just the size needed
+				regions[i].size = sizeToAllocate;
+				regions[i].available = FALSE;
+				if(remainingSize > 0){ //creates a new available region with the remaining size computed
+					for(int j = region_index; j > i; j--){ //shift all allocated regions one space to the right to make room for the newly created region
+						regions[j].base_address = regions[j-1].base_address;
+						regions[j].size = regions[j-1].size;
+						regions[j].available = regions[j-1].available;
+					}
+					regions[i+1].base_address = regions[region_index].base_address + regions[region_index].size;
+					regions[i+1].size = remainingSize;
+					regions[i+1].available = TRUE;
+					/* creates the new available region and set it to available*/
+					region_index++;
+				}
+				break;
+			}
+			else{ //allocated regions array is already full and thus no split is possible, simply mark the region as unavailable
+				regions[i].available = FALSE;
+				break;
+			}
 		}
-		
-		if(_size % PageTable::PAGE_SIZE > 0){
-			_size = ((_size / PageTable::PAGE_SIZE) + 1) * PageTable::PAGE_SIZE; //computes the size of the region, rounding to the next multiple of page size
-		}			
-		/* set the allocated region properties */
-		regions[region_index]->base_address = base_addr;
-		regions[region_index]->size = _size;
-		region_index++;
-		return base_addr;
 	}
+	
+	
+	
+	if(i == region_index){ //reached the last region allocated without finding any available spot
+		if(regions[region_index].available == TRUE && regions[i].size >= sizeToAllocate){ //check if the last allocated region is available and if it has enough size to accommodate the request 
+			base_addr = regions[region_index].base_address;
+			if(region_index < _MAX_NO_REGIONS - 1){ //there is still room to split this region in two if size needed is less than the current size of the region
+				regions[(region_index+1)].available = TRUE;
+				regions[(region_index+1)].base_address = regions[region_index].base_address + sizeToAllocate;
+				remainingSize = regions[region_index].size - sizeToAllocate;
+				regions[(region_index+1)].size = remainingSize;
+				/*creates a new region with the remaining size and set it to available*/
+				
+				regions[region_index].size = sizeToAllocate;
+				regions[region_index].available = FALSE;
+				regions[region_index].base_address = base_addr;
+				region_index++;
+				/*mark the region with the proper size and set available to false*/
+			}
+			else{ //allocated regions array is already full and thus no split is possible, simply mark the region as unavailable
+				regions[region_index].available = FALSE;
+			}
+		}
+		else{ //no region available to allocate
+			Console::puts("Error when allocating the region!\n");
+			return 0;
+		}
+	}
+	
+	return base_addr;
 }
 
 void VMPool::release(unsigned long _start_address){
-	int index = 0;
-	for (int i = 0; i < region_index; ++i) //search for the region that is going to be released and store it on the variable region
+	int index = -1;
+	for (int i = 0; i < region_index; ++i) //search for the region that is going to be released and store the index of it
 	{
-		if(regions[i]->base_address == _start_address){
+		if(regions[i].base_address == _start_address){
 			index = i;
 			break;
 		}
 	}
-
-	for (int i = _start_address; i < regions[i]->base_address + regions[i]->size; i += PageTable::PAGE_SIZE) //free all the pages within that region
-	{
-		pageTable->free_page(_start_address / PageTable::PAGE_SIZE);
+	if(index != -1) {
+		for (int i = _start_address; i < regions[i].base_address + regions[i].size; i += PageTable::PAGE_SIZE) //free all the pages within that region
+		{
+			pageTable->free_page(_start_address / PageTable::PAGE_SIZE);
+		}
+		
+		regions[index].available = TRUE; //mark the region as available
+		pageTable->load(); //flush the TLB
 	}
-
-	for (int i = index + 1; i < region_index; ++i) //shift all the regions after the one released one position back on the array
-	{
-		regions[i-1]->base_address = regions[i]->base_address;
-		regions[i-1]->size = regions[i]->size;
-	}
-	region_index--;
-	pageTable->load();
 }
 
 BOOLEAN VMPool::is_legitimate(unsigned long _address){
-	for (int i = 0; i < region_index; ++i) //iterate over the allocated regions to look if the address is valid
+	for (int i = 0; i < _MAX_NO_REGIONS; ++i) //iterate over the allocated regions to look if the address is valid
 	{
-		if(_address >= regions[i]->base_address && _address < (regions[i]->base_address + regions[i]->size)){ //_address is within the region
+		if(_address >= regions[i].base_address && _address < (regions[i].base_address + regions[i].size) && regions[i].available == FALSE){ //_address is within the region and region is not available
 			return TRUE;
 		}
 	}
