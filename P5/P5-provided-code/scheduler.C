12b0c2caba08e0f66dc832a6e9edc625e9161fc1
caioddm <caioduarte.diniz@gmail.com>
2016-03-23 14:43:41 -0500

    [P5] Initial commit

---
+++ P5/P5-provided-code/scheduler.C
@@ -0,0 +1,21 @@
+#include "scheduler.H"
+
+Scheduler::Scheduler(){
+
+}
+
+void Scheduler::yield(){
+
+}
+
+void Scheduler::resume(Thread * _thread){
+
+}
+
+void Scheduler::add(Thread * _thread){
+	
+}
+
+void Scheduler::terminate(Thread * _thread){
+	
+}
